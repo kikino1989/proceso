08be1d003f348218c388ae947345fae8dc0dcd34
enrique <kikino1989@yahoo.com>
2019-10-08 22:35:58 -0400

    created base service class

---
+++ src/app/libs/base.service.ts
@@ -0,0 +1,104 @@
+
+import { Observable, of } from 'rxjs';
+
+interface Condition {
+    operator?: "=" | "!=" | ">" | "<" | "<=" | ">="; 
+    joint: "OR" | "AND" | "NOR";
+}
+
+export default class BaseService<T> {
+    protected entities: any;
+    all(conditions?: Condition | Condition[]): Observable<T[]> {
+        if (window.cordova) {
+            // do sqlite
+        } else {
+            if (Array.isArray(conditions)) {
+                // do array
+            } else {
+                return of(this.entities.filter(budget => {
+                    const matches = [];
+                    for (const key in conditions) {
+                        if (budget.hasOwnProperty(key)) {
+                            matches.push(budget[key] === conditions[key] ? 1 : 0);
+                        }
+                    }
+                    return matches.reduce((match, cur) => match + cur) === matches.length;
+                }));
+            }
+            return of(this.entities);
+        }
+    }
+
+    one(conditions?: number | Condition | Condition[]): Observable<T>  {
+        if (window.cordova) {
+            // do sqlite
+        } else {
+            if (Array.isArray(conditions)) {            
+
+            } else if (typeof conditions === 'object') {
+
+            } else 
+                return of(this.entities.filter(budget => budget.id == conditions)[0]);
+        }
+    }
+
+    update(entity: T, data: T, conditions?: Condition | Condition[] ) {
+        for (const prop in data) {
+            if (entity.hasOwnProperty(prop)) {
+                entity[prop] = data[prop];
+            }
+        }
+        if (window.cordova) {
+            // save to the database
+        }
+    }
+
+    delete(conditions?: number | Condition | Condition[]) {
+        let entity;
+        if (typeof conditions === 'number') {
+            entity = this.entities.filter(entity => entity.id == conditions)
+        } else if (Array.isArray(conditions)) {
+
+        } else {
+
+        }
+        const index = this.entities.indexOf(entity);
+        this.entities.splice(index, 1);
+        if (window.cordova) {
+
+        }
+
+    }
+
+    insert(entity: T) {
+        if (window.cordova) {
+            // save to the database
+        } else {
+            this.entities.push(entity);
+        }
+
+    }
+
+    evalConditions(entity: T, conditions: Condition[]): boolean {
+        const matches = [];
+        for (const condition of conditions) {
+            const match = this.evalCondition(entity, condition);
+            if (condition.joint === 'OR' && match) {
+                return true;
+            }
+            matches.push(match ? 1 : 0);
+        }
+
+        return matches.reduce((value, cur) => value + cur) === matches.length; 
+    }
+
+    evalCondition(entity: T, condition: Condition): boolean {
+        const matches = [];
+        for (const key in condition) {
+            if (entity.hasOwnProperty(key)) {
+                matches.push(entity[key] === entity[key] ? 1 : 0);
+            }
+        }
+        return matches.reduce((match, cur) => match + cur) === matches.length;
+    }
+}
--- src/app/services/budgets.service.ts
+++ src/app/services/budgets.service.ts
@@ -7,65 +7,35 @@ import {budgets} from './testdata';
 import { BackgroundMode } from '@ionic-native/background-mode/ngx';
 import moment from 'moment';
 import * as _ from 'lodash';
+import BaseService from '../libs/base.service';
 
 const HOUR_PERIOD = 1000 * 60 * 60;
 
 @Injectable({
     providedIn: 'root',
 })
-export default class BudgetsService {
+export default class BudgetsService extends BaseService<Budget> {
 
-    constructor(private bm: BackgroundMode) { }
+    constructor(private bm: BackgroundMode) { super(); this.entities = budgets; }
 
-    getBudgets(condition: any = false): Observable<Budget[]> {
-        if (window.cordova) {
-            // do sqlite
-        } else {
-            if (condition) {
-                return of(budgets.filter(budget => {
-                    const matches = [];
-                    for (const key in condition) {
-                        if (budget.hasOwnProperty(key)) {
-                            matches.push(budget[key] === condition[key] ? 1 : 0);
-                        }
-                    }
-                    return matches.reduce((match, cur) => match + cur) === matches.length;
-                }));
-            }
-            return of(budgets);
-        }
+    getBudgets(condition: object | object[]): Observable<Budget[]> {
+        return this.all(condition);
     }
 
     getBudget(id: number): Observable<Budget> {
-        if (window.cordova) {
-            // do sqlite
-        } else {
-            return of(budgets.filter(budget => budget.id == id)[0]);
-        }
+        return this.one(id);
     }
 
     createBudget(budget: Budget) {
-        if (window.cordova) {
-            // save to the database
-        } else {
-            budgets.push(budget);
-        }
+        this.insert(budget);
     }
 
     updateBudget(budget: Budget, data: any) {
-        for (const prop in data) {
-            if (budget.hasOwnProperty(prop)) {
-                budget[prop] = data[prop];
-            }
-        }
-        if (window.cordova) {
-            // save to the database
-        }
+        this.update(budget, data);
     }
 
     deleteBudget(budget: Budget) {
-        const index = budgets.indexOf(budget);
-        budgets.splice(index, 1);
+        this.delete(budget.id);
     }
 
     async watchBudget(today = moment().format('Do')) {
@@ -90,10 +60,11 @@ export default class BudgetsService {
     async takeSnapshot() {
         const activeBudget = await this.getActiveBudget();
         const snapshot = _.cloneDeep(activeBudget);
-        snapshot.snapshot = moment().format('MM-DD-YYYY');
+        snapshot.snapshot = moment().subtract(1, 'day').format('MM-DD-YYYY');
         snapshot.parentID = activeBudget.id;
         snapshot.id++;
         this.createBudget(snapshot);
+        activeBudget.reset();
     }
 
     async snapshotForThisMonth(): Promise<boolean> {
